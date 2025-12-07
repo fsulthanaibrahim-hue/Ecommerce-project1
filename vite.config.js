e59317a3886f26abcdc5e5d19658c7b92c6c3217
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2025-12-06 17:09:29 -0800

    zias-ecom

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite';
+
 
 // https://vite.dev/config/
 export default defineConfig({
